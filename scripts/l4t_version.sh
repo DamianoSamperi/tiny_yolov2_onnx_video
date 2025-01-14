22ea0ce5ae7d997c04e991a1dfcbe8ab62194c87
DamianoSamperi <102314677+DamianoSamperi@users.noreply.github.com>
2025-01-14 10:48:23 +0100

    Update l4t_version.sh

--- scripts/l4t_version.sh
+++ scripts/l4t_version.sh
@@ -24,26 +24,27 @@
 L4T_VERSION_STRING=$(head -n 1 /etc/nv_tegra_release)
 
 if [ -z "$L4T_VERSION_STRING" ]; then
-	echo "reading L4T version from \"dpkg-query --show nvidia-l4t-core\""
-
-	L4T_VERSION_STRING=$(dpkg-query --showformat='${Version}' --show nvidia-l4t-core)
-	L4T_VERSION_ARRAY=(${L4T_VERSION_STRING//./ })	
-
-	#echo ${L4T_VERSION_ARRAY[@]}
-	#echo ${#L4T_VERSION_ARRAY[@]}
-
-	L4T_RELEASE=${L4T_VERSION_ARRAY[0]}
-	L4T_REVISION=${L4T_VERSION_ARRAY[1]}
+    echo "reading L4T version from \"dpkg-query --show nvidia-l4t-core\""
+    L4T_VERSION_STRING=$(dpkg-query --showformat='${Version}' --show nvidia-l4t-core)
+    L4T_VERSION_ARRAY=(${L4T_VERSION_STRING//./ })    
 else
-	echo "reading L4T version from /etc/nv_tegra_release"
-
-	L4T_RELEASE=$(echo $L4T_VERSION_STRING | cut -f 2 -d ' ' | grep -Po '(?<=R)[^;]+')
-	L4T_REVISION=$(echo $L4T_VERSION_STRING | cut -f 2 -d ',' | grep -Po '(?<=REVISION: )[^;]+')
+    echo "reading L4T version from /etc/nv_tegra_release"
+    L4T_RELEASE=$(echo $L4T_VERSION_STRING | cut -f 2 -d ' ' | grep -Po '(?<=R)[^;]+')
+    L4T_REVISION=$(echo $L4T_VERSION_STRING | cut -f 2 -d ',' | grep -Po '(?<=REVISION: )[^;]+')
 fi
 
 L4T_REVISION_MAJOR=${L4T_REVISION:0:1}
 L4T_REVISION_MINOR=${L4T_REVISION:2:1}
-
 L4T_VERSION="$L4T_RELEASE.$L4T_REVISION"
 
 echo "L4T BSP Version:  L4T R$L4T_VERSION"
+
+# Check if the L4T version is 24.7.6, and change behavior accordingly
+if [ "$L4T_VERSION" == "24.7.6" ]; then
+    echo "Detected L4T version 24.7.6. Switching to use r32.7.1."
+    # Aggiungi qui le operazioni da fare se la versione è 24.7.6
+    # Ad esempio, potresti voler usare una specifica versione del software
+    L4T_VERSION="32.7.1"
+else
+    echo "Using L4T version: $L4T_VERSION"
+fi
